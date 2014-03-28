da3b81f08d16eceb530c8e04b516dcf706c4dcbd
sato_daisuke <sato_daisuke@cyberagent.co.jp>
2014-03-28 17:35:44 +0900

    added animation like smartnews

--- src/com/eccyan/bootcamp/TabPageTransformer.java
+++ src/com/eccyan/bootcamp/TabPageTransformer.java
@@ -1,27 +1,34 @@
+
 package com.eccyan.bootcamp;
 
-import android.graphics.drawable.Drawable;
 import android.support.v4.view.ViewPager.PageTransformer;
 import android.view.View;
 import android.widget.ScrollView;
-import com.nineoldandroids.*;
+import com.nineoldandroids.view.ViewHelper;;
+
+;
 
 public class TabPageTransformer implements PageTransformer {
-    
+    private static float MIN_SCALE = 0.75f;
+
     public TabPageTransformer() {
         super();
     }
 
     @Override
     public void transformPage(View page, float position) {
-        /*
-        ScrollView scrollView = (ScrollView)page.findViewById(R.id.card_scroll_view);
-        Drawable background = (Drawable)scrollView.getBackground();
-        
-        if (position >= 0) {
-            background.setAlpha((int)(0xff * (1 - position)));
+        ScrollView scrollView = (ScrollView) page.findViewById(R.id.card_scroll_view);
+        int pageWidth = scrollView.getWidth();
+
+        if (position < -1) {
+            ViewHelper.setAlpha(scrollView, 0);
+        } else if (position <= 0) {
+            ViewHelper.setTranslationX(scrollView, pageWidth * -position);
+        } else if (position <= 1) {
+            ViewHelper.setTranslationX(scrollView, pageWidth * -position);
+        } else { 
+            ViewHelper.setAlpha(scrollView, 0);
         }
-        */
     }
 
 }
