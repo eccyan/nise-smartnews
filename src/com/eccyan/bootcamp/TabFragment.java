742a5db6b461f0eb921283eba6249216cf7da73d
sato_daisuke <sato_daisuke@cyberagent.co.jp>
2014-03-28 10:15:49 +0900

    add padding & changed click to touch event

--- src/com/eccyan/bootcamp/TabFragment.java
+++ src/com/eccyan/bootcamp/TabFragment.java
@@ -3,19 +3,23 @@ package com.eccyan.bootcamp;
 
 import java.util.Arrays;
 
+import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class TabFragment extends Fragment
-        implements OnClickListener {
+        implements OnTouchListener {
     public static final String TITLE = "title";
     public static final String COLOR = "color";
 
@@ -38,6 +42,15 @@ public class TabFragment extends Fragment
         GradientDrawable gradientDrawable = (GradientDrawable) layerDrawable.getDrawable(1);
         gradientDrawable.setColor(color);
     }
+    
+    private static int getPressedColor(int color) {
+        int alpha = Color.alpha(color);
+        int red = Color.red(color);
+        int green = Color.green(color);
+        int blue = Color.blue(color);
+        
+        return Color.argb(alpha, (red + 0x1f) / 2, (green + 0x1f) / 2, (blue + 0x1f) / 2);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -50,23 +63,18 @@ public class TabFragment extends Fragment
 
         setBackGroundColorForTextView(textView, arguments.getInt(COLOR));
 
-        textView.setOnClickListener(this);
+        textView.setOnTouchListener(this);
 
         return textView;
-    }
-
-    @Override
-    public void onClick(View view) {
-        ViewPager viewPager = (ViewPager)getActivity().findViewById(R.id.pager);
-        String[] categoryNames = getResources().getStringArray(R.array.category_names);
-        
-        int position = Arrays.asList(categoryNames).indexOf(getArguments().getString(TITLE));
-        viewPager.setCurrentItem(position);
     }
     
     public void setBackgroundToDefault() {
         TextView tabView = (TextView)getView().findViewById(R.id.tab_text_view);
         tabView.setBackgroundResource(R.drawable.background_tab_shape);
+        
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        
+        tabView.setPadding(0, (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, displayMetrics), 0, 0);
         setBackGroundColorForTextView(tabView, getArguments().getInt(COLOR));
     }
     
@@ -74,6 +82,38 @@ public class TabFragment extends Fragment
         TextView tabView = (TextView)getView().findViewById(R.id.tab_text_view);
         tabView.setBackgroundResource(R.drawable.background_tab_shape_focused);
         setBackGroundColorForTextView(tabView, getArguments().getInt(COLOR));
+    }
+    
+    public void setBackgroundToPressed() {
+        TextView tabView = (TextView)getView().findViewById(R.id.tab_text_view);
+        setBackGroundColorForTextView(tabView, getPressedColor(getArguments().getInt(COLOR)));
+    }
+    
+    public void setBackgroundToNotPressed() {
+        TextView tabView = (TextView)getView().findViewById(R.id.tab_text_view);
+        setBackGroundColorForTextView(tabView, getArguments().getInt(COLOR));
+    }
+    
+    public void setViewPagerItemToTabPosition() {
+        ViewPager viewPager = (ViewPager)getActivity().findViewById(R.id.pager);
+        String[] categoryNames = getResources().getStringArray(R.array.category_names);
+        
+        int position = Arrays.asList(categoryNames).indexOf(getArguments().getString(TITLE));
+        viewPager.setCurrentItem(position);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        setBackgroundToNotPressed();
+        
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            setBackgroundToPressed();
+        }
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            setViewPagerItemToTabPosition();
+        }
+        
+        return true;
     }
     
 }
